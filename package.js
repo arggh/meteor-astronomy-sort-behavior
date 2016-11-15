2b77124f60aa32b38094f6476f2ab2fdda23c88b
arggh <alterilari@gmail.com>
2016-11-15 15:16:21 +0200

    Bump versions

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
   summary: 'Sort behavior for Meteor Astronomy',
-  version: '2.0.0',
+  version: '2.2.0',
   name: 'jagi:astronomy-sort-behavior',
   git: 'https://github.com/jagi/meteor-astronomy-sort-behavior.git'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3');
+  api.versionsFrom('1.4.2.1');
 
   api.use([
     'ecmascript',
     'es5-shim',
-    'jagi:astronomy@2.0.0',
+    'jagi:astronomy@2.3.3',
     'underscore'
   ], ['client', 'server']);
 
